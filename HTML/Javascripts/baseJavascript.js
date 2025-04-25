132309f1a1e69f6931e795eef6d2662452592049
Prateek Singh <prateeksingh200@gmail.com>
2025-04-25 10:50:47 +0530

    25Apr-update-prateek

--- HTML/Javascripts/baseJavascript.js
+++ HTML/Javascripts/baseJavascript.js
@@ -3,7 +3,7 @@ var tooltips = document.querySelectorAll('.tool_tip span');
 
 window.onmousemove = function (e) {
     var x = (e.clientX + 0) + 'px',
-        y = (e.clientY + 48) + 'px';
+        y = (e.clientY + 24) + 'px';
     for (var i = 0; i < tooltips.length; i++) {
         tooltips[i].style.top = y;
         tooltips[i].style.left = x;
@@ -34,3 +34,11 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const layout = document.getElementById('layout');
+    sidebar.classList.toggle('collapsed');
+    layout.classList.toggle('collapsed');
+  }
