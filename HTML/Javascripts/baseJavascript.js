378499add7969616760901687cd834e25fd034e4
Prateek Singh <prateeksingh200@gmail.com>
2025-04-30 15:47:47 +0530

    30 APR_3files

--- HTML/Javascripts/baseJavascript.js
+++ HTML/Javascripts/baseJavascript.js
@@ -16,3 +16,68 @@ function toggleSidebar() {
     sidebar.classList.toggle('collapsed');
     layout.classList.toggle('collapsed');
   }
+
+// // Chips functionality
+// const dummyTags = ["Java", "Spring", "Hibernate", "Java Beans", "SQL", "Python"];
+
+// const chipsContainer = document.getElementById('chipsContainer');
+// const chipInput = document.getElementById('chipInput');
+
+// let selectedChips = [];
+
+// function renderChips() {
+//   chipsContainer.innerHTML = '';
+
+//   let displayChips = selectedChips.slice(0, 4);
+//   let extraCount = selectedChips.length - 4;
+
+//   displayChips.forEach((chip, index) => {
+//     const chipEl = document.createElement('div');
+//     chipEl.className = 'chip';
+//     chipEl.innerHTML = `${chip} <span class="close-btn" onclick="removeChip(${index})">&times;</span>`;
+//     chipsContainer.appendChild(chipEl);
+//   });
+
+//   if (extraCount > 0) {
+//     const badge = document.createElement('div');
+//     badge.className = 'badge';
+//     badge.innerText = `${extraCount}+`;
+//     chipsContainer.appendChild(badge);
+//   }
+// }
+
+// function removeChip(index) {
+//   selectedChips.splice(index, 1);
+//   renderChips();
+// }
+
+// chipInput.addEventListener('keydown', function (e) {
+//   if (e.key === 'Enter' && chipInput.value.trim() !== '') {
+//     e.preventDefault();
+//     if (!selectedChips.includes(chipInput.value.trim())) {
+//       selectedChips.push(chipInput.value.trim());
+//     }
+//     chipInput.value = '';
+//     renderChips();
+//   }
+// });
+
+// setTimeout(() => {
+//   selectedChips = dummyTags.slice(0, 5);
+//   renderChips();
+// }, 300);
+
+// function updateLabelState() {
+//     const chipContainer = document.querySelector('.chip-container');
+//     const chips = document.querySelectorAll('.chip');
+    
+//     if (chips.length > 0) {
+//       chipContainer.classList.add('has-value');
+//     } else {
+//       chipContainer.classList.remove('has-value');
+//     }
+//   }
+  
+//   // Call this function whenever you add/remove chips
+//   updateLabelState();
+  
